0d45428e1b3ad3a68b9d39df03b551eadf81634f
梁英健 <lyj@LYJ-MacBook-Pro.local>
2019-03-26 16:59:11 +0800

    add douban popular movies spider

---
+++ douban/douban_spider.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import json
+
+import requests
+
+# 定义请求url
+url = "https://movie.douban.com/j/search_subjects"
+
+# 定义请求头
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36"
+}
+# 循环构建请求参数并且发送请求
+for page_start in range(0, 100, 20):
+    params = {
+        "type": "movie",
+        "tag": "热门",
+        "sort": "recommend",
+        "page_limit": "20",
+        "page_start": page_start
+    }
+    response = requests.get(
+        url=url,
+        headers=headers,
+        params=params
+    )
+
+    # 方式一 直接转换json方法
+    # results = response.json()
+    # 方式二：手动转换
+    # 获取字符串
+    content = response.content
+    # 转换成字符串
+    results = json.loads(content)
+
+    # 解析结果
+    for movie in results["subjects"]:
+        print(movie["title"], movie["rate"])
